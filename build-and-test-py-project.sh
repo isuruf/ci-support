20045c9dff4237aa6618c8abdfd9839374464f37
Andreas Kloeckner <inform@tiker.net>
2013-06-09 17:42:45 -0400

    Fix pip PATH fix

--- build-and-test-py-project.sh
+++ build-and-test-py-project.sh
@@ -17,7 +17,7 @@ curl -k https://bitbucket.org/pypa/setuptools/raw/0.7.2/ez_setup.py | python -
 curl -k https://raw.github.com/pypa/pip/master/contrib/get-pip.py | python -
 
 # Not sure why the hell pip ends up there, but in Py3.3, it sometimes does.
-export PATH=$PATH:`pwd`/.env/local/bin
+export PATH=`pwd`/.env/local/bin:$PATH
 
 if test -f requirements.txt; then
   pip install -r requirements.txt

e95930c5400e8151320561c6544364f9fe4fb01b
Andreas Kloeckner <inform@tiker.net>
2015-05-05 01:50:34 -0500

    Pip invocation fixes

--- build-and-test-py-project.sh
+++ build-and-test-py-project.sh
@@ -31,16 +31,18 @@ if test "$py_version" = "2.5"; then
   hash -r
   which easy_install
   easy_install 'pip==1.3.1'
-  PIP="pip --insecure"
+  PIP_FLAGS="--insecure"
 else
   #curl -k https://raw.github.com/pypa/pip/1.4/contrib/get-pip.py | python -
   curl http://git.tiker.net/pip/blob_plain/77f959a3ce9cc506efbf3a17290d387d0a6624f5:/contrib/get-pip.py | python -
 
-  PIP="pip"
+  PIP_FLAGS=""
 fi
 
 # Not sure why the hell pip ends up there, but in Py3.3, it sometimes does.
 export PATH=`pwd`/.env/local/bin:$PATH
+
+PIP="${py_exe} $(which pip) $PIP_FLAGS"
 
 if test "$EXTRA_INSTALL" != ""; then
   for i in $EXTRA_INSTALL ; do

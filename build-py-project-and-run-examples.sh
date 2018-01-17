5ebec6904ee445b446d0f6320cbd16a670a84c53
Andreas Kloeckner <inform@tiker.net>
2018-01-16 21:29:41 -0600

    Fix examples runner

--- build-py-project-and-run-examples.sh
+++ build-py-project-and-run-examples.sh
@@ -5,9 +5,10 @@ set -e
 curl -L -O -k https://gitlab.tiker.net/inducer/ci-support/raw/master/build-py-project.sh
 source build-py-project.sh
 
-for i in $(find examples -name '*.py' -exec grep -q __main__ '{}' \; -print ); do
+cd examples
+for i in $(find . -name '*.py' -exec grep -q __main__ '{}' \; -print ); do
   echo "-----------------------------------------------------------------------"
   echo "RUNNING $i"
   echo "-----------------------------------------------------------------------"
-  ${PY_EXE} "$i$"
+  ${PY_EXE} "$i"
 done

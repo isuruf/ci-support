f3e1b18d98ee594ab263878dfd7902030c6823d8
Andreas Kloeckner <inform@tiker.net>
2018-07-27 15:12:15 -0500

    Actually build project for pylint

--- prepare-and-run-pylint.sh
+++ prepare-and-run-pylint.sh
@@ -1,41 +1,13 @@
 #! /bin/bash
 
 set -e
-set -x
 
-PY_EXE=python3.6
-
-echo "-----------------------------------------------"
-echo "Current directory: $(pwd)"
-echo "Python executable: ${PY_EXE}"
-echo "-----------------------------------------------"
-
-# {{{ clean up
-
-rm -Rf .env
-rm -Rf build
-find . -name '*.pyc' -delete
-
-rm -Rf env
-git clean -fdx -e siteconf.py -e boost-numeric-bindings -e local_settings.py
-
-if test `find "siteconf.py" -mmin +1`; then
-  echo "siteconf.py older than a minute, assumed stale, deleted"
-  rm -f siteconf.py
+if [ "$PY_EXE" == "" ]; then
+  PY_EXE=python${py_version}
 fi
 
-# }}}
-
-git submodule update --init --recursive
-
-# {{{ virtualenv
-
-${PY_EXE} -m venv .env
-. .env/bin/activate
-
-${PY_EXE} -m ensurepip
-
-# }}}
+curl -L -O -k https://gitlab.tiker.net/inducer/ci-support/raw/master/build-py-project.sh
+source build-py-project.sh
 
 $PY_EXE -m pip install pylint
 

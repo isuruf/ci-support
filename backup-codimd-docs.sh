704d2351e4b067de45a196355df00e8f58963dc4
Andreas Kloeckner <inform@tiker.net>
2020-05-10 00:05:10 -0500

    Fix path for CodiMD CLI tool

--- backup-codimd-docs.sh
+++ backup-codimd-docs.sh
@@ -4,9 +4,9 @@ apt-get update
 apt-get install -y jq curl git
 
 # pending https://github.com/codimd/cli/pull/36
-git clone https://github.com/inducer/codimd-cli.git
+git clone https://github.com/inducer/codimd-cli.git codimd-cli
 
-CODIMD=$(pwd)/cli/bin/codimd
+CODIMD=$(pwd)/codimd-cli/bin/codimd
 export CODIMD_SERVER='https://codimd.tiker.net'
 $CODIMD login --email inform+codibackup@tiker.net "$CODIMD_PASSWORD"
 while read -r DOCID FILEPATH; do

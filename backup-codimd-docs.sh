32c1a4049a82d13c6ba4d398a17c6f30f1e646a6
Andreas Kloeckner <inform@tiker.net>
2020-05-09 23:31:20 -0500

    First bit of codimd backup script

---
+++ backup-codimd-docs.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+sudo apt update
+sudo apt install jq curl
+git clone https://github.com/codimd/cli.git
+
+CODIMD=$(pwd)/cli/bin/codimd
+export CODIMD_SERVER='https://codimd.tiker.net'
+$CODIMD login inform+codibackup@tiker.net "$CODIMD_PASSWORD"
+while read -r DOCID FILEPATH; do
+    echo "Reading note $DOCID into $FILEPATH"
+    codimd export --md "$DOCID" "$FILEPATH"
+done < .codi-backup.txt

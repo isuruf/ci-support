e2cd884f56b645994a38c25c020987f6ebb6076f
Andreas Kloeckner <inform@tiker.net>
2020-05-09 23:50:08 -0500

    CodiMD: fix login command

--- backup-codimd-docs.sh
+++ backup-codimd-docs.sh
@@ -6,7 +6,7 @@ git clone https://github.com/codimd/cli.git
 
 CODIMD=$(pwd)/cli/bin/codimd
 export CODIMD_SERVER='https://codimd.tiker.net'
-$CODIMD login inform+codibackup@tiker.net "$CODIMD_PASSWORD"
+$CODIMD login --email inform+codibackup@tiker.net "$CODIMD_PASSWORD"
 while read -r DOCID FILEPATH; do
     echo "Reading note $DOCID into $FILEPATH"
     codimd export --md "$DOCID" "$FILEPATH"

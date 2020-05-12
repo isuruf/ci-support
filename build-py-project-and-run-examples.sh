107ceeded4a1de106e29e33e15cf53b612f3b20e
Andreas Kloeckner <inform@tiker.net>
2020-05-12 16:30:06 -0500

    Examples runner: allow conda build

--- build-py-project-and-run-examples.sh
+++ build-py-project-and-run-examples.sh
@@ -6,8 +6,13 @@ if [ "$PY_EXE" == "" ]; then
   PY_EXE=python${py_version}
 fi
 
-curl -L -O -k https://gitlab.tiker.net/inducer/ci-support/raw/master/build-py-project.sh
-source build-py-project.sh
+if test "$USE_CONDA_BUILD" == "1"; then
+  curl -L -O -k "${ci_support}/build-py-project-within-miniconda.sh"
+  source build-py-project-within-miniconda.sh
+else
+  curl -L -O -k "${ci_support}/build-py-project.sh"
+  source build-py-project.sh
+fi
 
 curl -L -O -k https://gitlab.tiker.net/inducer/ci-support/raw/master/run-examples.sh
 source run-examples.sh

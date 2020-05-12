da6748c2fd67ee4a35024dd0246ab3f70f9994d9
Andreas Kloeckner <inform@tiker.net>
2020-05-12 16:47:52 -0500

    Add missing env var to examples runner

--- build-py-project-and-run-examples.sh
+++ build-py-project-and-run-examples.sh
@@ -6,6 +6,8 @@ if [ "$PY_EXE" == "" ]; then
   PY_EXE=python${py_version}
 fi
 
+ci_support="https://gitlab.tiker.net/inducer/ci-support/raw/master"
+
 if test "$USE_CONDA_BUILD" == "1"; then
   curl -L -O -k "${ci_support}/build-py-project-within-miniconda.sh"
   source build-py-project-within-miniconda.sh

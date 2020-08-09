e2b4b05d604bcfe7726c1f6e8bc8d2c488047ff0
Andreas Kloeckner <inform@tiker.net>
2020-08-09 09:18:57 -0500

    Do not use sphinx 3.2 because of bug

--- build-docs.sh
+++ build-docs.sh
@@ -5,7 +5,8 @@ source ci-support.sh
 
 build_py_project_in_venv
 
-$PY_EXE -m pip install docutils sphinx
+# https://github.com/sphinx-doc/sphinx/issues/8084
+$PY_EXE -m pip install docutils "sphinx<3.2"
 
 cd doc
 

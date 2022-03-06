50c181be5aa2fc7eeaff347ff1c8882ab0dfdb09
Andreas Kloeckner <inform@tiker.net>
2022-03-06 17:12:24 -0600

    ISPC install: fall back to release if appveyor limit exceeded

--- ci-support.sh
+++ ci-support.sh
@@ -683,11 +683,14 @@ function edit_requirements_txt_for_downstream_in_subdir()
 
 function install_ispc()
 {
-    curl -L "https://ci.appveyor.com/api/projects/ispc/ispc/artifacts/build%2Fispc-trunk-linux.tar.gz?job=Environment%3A%20APPVEYOR_BUILD_WORKER_IMAGE%3DUbuntu1804%2C%20LLVM_VERSION%3Dlatest" | tar xfz -
-    PATH="$(pwd)/ispc-trunk-linux/bin:$PATH"
-
-    # curl -L https://github.com/ispc/ispc/releases/download/v1.17.0/ispc-v1.17.0-linux.tar.gz  | tar xfz -
-    # PATH="$(pwd)/ispc-v1.17.0-linux/bin:$PATH"
+    if (curl -L "https://ci.appveyor.com/api/projects/ispc/ispc/artifacts/build%2Fispc-trunk-linux.tar.gz?job=Environment%3A%20APPVEYOR_BUILD_WORKER_IMAGE%3DUbuntu1804%2C%20LLVM_VERSION%3Dlatest" \
+        | tar xfz - ) ; then
+      # Appveyor downloads can fail if the daily download limit is exceeded.
+      PATH="$(pwd)/ispc-trunk-linux/bin:$PATH"
+    else
+      curl -L https://github.com/ispc/ispc/releases/download/v1.17.0/ispc-v1.17.0-linux.tar.gz  | tar xfz -
+      PATH="$(pwd)/ispc-v1.17.0-linux/bin:$PATH"
+    fi
 
     export PATH
 }

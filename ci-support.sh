45d140df3d1d8901b50a6102e1a90ef7db917861
Andreas Kloeckner <inform@tiker.net>
2023-05-10 17:58:52 -0500

    Revert "conda build: Install cairo if gmsh is required"
    This reverts commit 1281b1c4a4bb0994404c49fb5b8337fc7482dbac.

--- ci-support.sh
+++ ci-support.sh
@@ -236,11 +236,6 @@ install_conda_deps()
     CONDA_EXE_DIR=$MINIFORGE_INSTALL_DIR/bin
   fi
 
-  if [ $PLATFORM = "Linux" ]; then
-    # https://github.com/conda-forge/gmsh-feedstock/issues/74
-    sed -i '/gmsh/i- cairo' "$CONDA_ENVIRONMENT"
-  fi
-
   PATH="$CONDA_EXE_DIR:$PATH" with_echo conda update conda --yes --quiet
   PATH="$CONDA_EXE_DIR:$PATH" with_echo conda update --all --yes --quiet
   PATH="$CONDA_EXE_DIR:$PATH" with_echo conda env create --file "$CONDA_ENVIRONMENT" --name testing --quiet
